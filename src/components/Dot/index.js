f77dc94c96a8018417be6da593318d82e4e9bfe3
Vitor Nogueira <vslnogueira96@gmail.com>
2023-01-23 11:32:14 -0300

    feat: criando componente dot (que será nossos circulos com as opções de cores dos tênis

---
+++ src/components/Dot/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+
+export default function Dot({ color }) {
+  return <View style={[styles.container, { backgroundColor: color }]}></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: Dimensions.get("window").width * 0.05,
+    height: Dimensions.get("window").width * 0.05,
+    borderRadius: (Dimensions.get("window").width * 0.05) / 2,
+    marginHorizontal: "2.5%",
+    elevation: 5,
+  },
+});
